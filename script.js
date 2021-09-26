8383b2a0a3749d87aac40308b24cd59f11b3310f
Jonnyb83 <64705009+Jonnyb83@users.noreply.github.com>
2021-09-26 20:20:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function change_text(){
+    document.getElementById("p1").innerHTML = "You clicked the button, I am new paragraph.";
+}
+
+function change_text(){
+    document.getElementById("p2").innerHTML = "New text come hier";
+}
+
+
+// type="text/javascript";
+
+// document.getElementById("p1").innerHTML = "Amateur film production in Norh of Slovakia has started in years 90ties it was 8mm film and
+//               nowadays has
+//               amateur film production expanded to production mady by smartphone so anyone can make movies and easly.
+//               Mostly everbody have quit good camera by the hand in his smartphone. But how you can use it??? You can
+//               learn in this website and here you can check other amateurs videos also.";
+
+
+// var x = document.getElementById("news");
+// x.addEventListener("click", changeText);
+
+// function changeText(){
+// 	document.getElementById('p1').style.visibility='hidden'
+// 	document.getElementById('p2').style.visibility='visible'
+// 	document.getElementById('p3').style.visibility='hidden'
+// 	document.getElementById('p4').style.visibility='hidden'
+// 	document.getElementById('p5').style.visibility='hidden'
+// 	document.getElementById('p6').style.visibility='hidden'
+// 	document.getElementById('p7').style.visibility='hidden'
+// 	document.getElementById('p8').style.visibility='hidden'
+// 	document.getElementById('p9').style.visibility='hidden'
+// }
+
+
+
+// var button
+
+// document.getElementById("p1").addEventListener("click", function() {
+//   document.textContent()
+// });
